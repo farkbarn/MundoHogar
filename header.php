82e907558cfdc4d4810efc3778b4fad8882d3874
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2016-08-18 15:17:25 -0400

    agregando TM

--- header.php
+++ header.php
@@ -31,11 +31,13 @@
   ga('create', 'UA-82759205-1', 'auto');
   ga('send', 'pageview');
 </script>
-
-
-
-
-
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K5JBNN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5JBNN');</script>
 
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'oblique' ); ?></a>
